d9817c39748ac0f8978f30b582d32b11638177fd
Abdul Salam <abdulsalam17004@gmail.com>
2026-03-26 08:47:42 +0530

    Add files via upload

---
+++ luxeva trends {Abdul Salam}/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "fashion-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "shop.html",
+  "product-details.html",
+  "cart.html",
+  "styles/style.css",
+  "scripts/script.js",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
